0e53da2f92bd3219676fbf427bc0d57284f4b22c
Ozodbekinc <ozodbekllc@gmail.com>
2019-05-04 21:28:31 +0500

    minor changes implemented on database

--- resources/views/add_staff.blade.php
+++ resources/views/add_staff.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>L</title>
 <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"> -->
